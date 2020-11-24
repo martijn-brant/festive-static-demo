d7ee45adae9dc6c9b31beac00c0a07d02d8e38b1
Martijn Brant <65955176+martijn-brant@users.noreply.github.com>
2020-11-25 00:30:49 +0100

    Add more dumb gift API stuff

--- api/GetGifts/index.js
+++ api/GetGifts/index.js
@@ -1,6 +1,7 @@
 module.exports = async function (context, req) {
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: { text: '{"Xbox": "Microsoft"}' },
+    // Don't mind this crappy code:
+    body: { text: '{   "gifts": { "Xbox": "Microsoft Store", "OLED": "Big TV store", "Ori and the Will of the Wisps": "Video game store"  } }' },
   };
 };
