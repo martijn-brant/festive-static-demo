b618525e7687df097c81f80abf0c8a0d7678f42e
Martijn Brant <65955176+martijn-brant@users.noreply.github.com>
2020-11-25 21:02:45 +0100

    Updates to gift table

--- api/GetGifts/index.js
+++ api/GetGifts/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
   context.res = {
     // status: 200, /* Defaults to 200 */
-    // Don't mind this crappy code:
-    body: { text: '{   "gifts": { "Xbox": "Microsoft Store", "OLED": "Big TV store", "Ori and the Will of the Wisps": "Video game store"  } }' },
+    // Don't mind this crappy code, you should imagine some fancy database calling code here and format objects to JSON. You get the idea.
+    body: { text: '{    "gifts": {      "Xbox": {          "Name": "Xbox",          "Notes": "The new with the X!",          "Store": "Microsoft Store"      },      "0LED": {          "Name": "Ell-Geeh 0LED UHD",          "Notes": "64 inch monster!",          "Store": "Big TV store"      },      "Ori": {          "Name": "Ori and the Will of the Wisps",          "Notes": "The best platformer!",          "Store": "Video game store"      }    }  }  ' },
   };
 };
